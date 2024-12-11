00a9896386ab39632fa39adba29e4d0225bc7f79
Ansah Kelvin Nana Osei <ansahkelvin0@gmail.com>
2024-12-11 10:25:42 +0000

    React form implemented

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,15 @@
+"use client"
+import AuthForm from "@/components/forms/AuthForm";
+import {SignInSchema } from "@/lib/validations";
+
 
 const SignInPage = () => {
-    return (
-        <div>
-            Sign In
-        </div>
-    )
+    return <AuthForm schema={SignInSchema}
+                     formType={"SIGN_IN"}
+                     defaultValues={{ email: "", password: "" }}
+                     onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+
 }
 
 export default SignInPage
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,14 @@
+"use client"
+import AuthForm from "@/components/forms/AuthForm";
+import {SignUpSchema} from "@/lib/validations";
 
 const SignUpPage = () => {
-    return (
-        <div>
-            Sign Up
-        </div>
-    )
+    return <AuthForm
+        schema={SignUpSchema}
+        formType={"SIGN_UP"}
+        defaultValues={{ email: "", password: "", name: "", username: "" }}
+        onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
 }
 
 export default SignUpPage
