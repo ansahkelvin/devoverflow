42bb97986f4a4259b67ac33fa0802eb0d4bf3a97
Ansah Kelvin Nana Osei <ansahkelvin0@gmail.com>
2024-12-16 21:53:09 +0000

    Added some more routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,24 @@
-import {auth, signOut} from "@/auth";
+import Link from "next/link";
+
 import {Button} from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 
 export default async function Home() {
-    const session = await auth()
   return (
    <>
-       <h1>Hello world</h1>
-       <p className="mt-24 text-white">{session?.user?.email}</p>
-
-       <form action={
-          async () => {
-               "use server"
-               await signOut({
-                   redirectTo: ROUTES.SIGN_IN
-               });
-           }
-       }>
-          <Button>
-              Sign Out
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900" asChild>
+              <Link href={ROUTES.ASK_QUESTION}>Ask a question</Link>
           </Button>
-       </form>
+      </section>
+
+       <section className="mt-11">
+            LOCAL SEARCH
+       </section>
+
+
+       
 
    </>
   );
